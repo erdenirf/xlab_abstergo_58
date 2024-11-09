94f92615e799fb997f23ad4347fa7f521ce0246e
Erdeni Tsyrendashiev <erdenirf@gmail.com>
2024-11-09 12:02:44 +0300

    remove Queue Brokers

--- server/main.py
+++ server/main.py
@@ -1,42 +1,20 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
-from fastapi.responses import StreamingResponse
-from tasks import ai_model_get_filename, is_exist, get_filename, model_generate
-import uuid
+import time
 
 app = FastAPI()
 
+def model_generate(prompt: str) -> str:
 
-# @app.get("/audio/{id}")
-# async def get_file(id: str) -> FileResponse:
+    time.sleep(10)   #sec
 
-#     if not is_exist(id):
-#         raise HTTPException(status_code=404, detail=f"File not found. ID: {id}")
-
-#     filename = get_filename(id)    
-#     if filename is None:
-#         raise HTTPException(status_code=400, detail="File is processing. Wait a little bit.")
-    
-#     return FileResponse(filename)
-
-
-# @app.post("/generation/{prompt}")
-# async def generation(prompt: str) -> str:
-
-#     new_id = str(uuid.uuid4())
-#     ai_model_get_filename.delay(prompt, filename_id = new_id)
-
-#     return new_id
-
-
+    return "./16742513.mp3"
 
 
 @app.post("/generation/{prompt}", response_class=FileResponse)
 def generation_prompt(prompt: str) -> str:
-
-    new_id = str(uuid.uuid4())
     
-    return ai_model_get_filename(prompt, filename_id = new_id) #model_generate(prompt, new_id)
+    return model_generate(prompt)
 
 
 if __name__ == "__main__":
